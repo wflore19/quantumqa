56675802763e160dea83cfaea8b54d87d6b6f0bd
Wilfredo Flores <wflore19@montgomerycollege.edu>
2024-07-23 11:45:29 -0400

    fix: reject application param

--- packages/core/src/modules/application/helpers/reject-application.ts
+++ packages/core/src/modules/application/helpers/reject-application.ts
@@ -4,7 +4,7 @@ import { ApplicationStatus } from '@quantumqa/types';
 import { type GetBullJobData } from '@/infrastructure/bull/bull.types';
 
 export async function rejectApplication({
-  applicationId: string,
+  applicationId,
 }: GetBullJobData<'application.rejected'>) {
   await db
     .updateTable('application')
