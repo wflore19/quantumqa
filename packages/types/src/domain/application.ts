2582b76325ca55d17d981f717ecf3cbdb470c9f6
Wilfredo Flores <wflore19@montgomerycollege.edu>
2024-07-23 11:43:25 -0400

    fix: application type

--- packages/types/src/domain/application.ts
+++ packages/types/src/domain/application.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 
+import { Entity } from './types';
 import { type ExtractValue } from '../utils/types';
 
 // Enums
@@ -12,9 +13,25 @@ export const ApplicationStatus = {
 
 // Schemas
 
-export const Application = z.object({
-  id: z.string(),
-  name: z.string(),
+export const Application = Entity.extend({
+  firstName: z.string().trim().min(1).toLowerCase(),
+  lastName: z.string().trim().min(1).toLowerCase(),
+  fullName: z.string().trim().min(1).toLowerCase(),
+  email: z
+    .string()
+    .trim()
+    .min(1)
+    .toLowerCase()
+    .email()
+    .refine((value) => {
+      return (
+        value.endsWith('.edu') ||
+        value.endsWith('.com') ||
+        value.endsWith('gmail.com') ||
+        value.endsWith('outlook.com')
+      );
+    }, 'Must be a valid email.'),
+  gender: z.string().min(1).toLowerCase(),
   status: z.nativeEnum(ApplicationStatus),
 });
 
