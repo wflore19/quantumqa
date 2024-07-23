9add559e4ca7bfcee41035cd9a1a69bc57056e6a
Wilfredo Flores <wflore19@montgomerycollege.edu>
2024-07-23 11:41:53 -0400

    add: application worker

--- packages/core/src/modules/application/application.worker.ts
+++ packages/core/src/modules/application/application.worker.ts
@@ -2,27 +2,24 @@ import { match } from 'ts-pattern';
 
 import { ApplicationBullJob } from '@/infrastructure/bull/bull.types';
 import { registerWorker } from '@/infrastructure/bull/helpers/register-worker';
-import { onApplicationAccepted } from './events/application-accepted';
-import { onApplicationCreated } from './events/application-created';
-import { onApplicationRejected } from './events/application-rejected';
-import { reviewApplication } from './helpers/review-application.ts';
+import { acceptApplication } from './helpers/accept-application';
+import { createApplication } from './helpers/create-application';
+import { rejectApplication } from './helpers/reject-application';
+// import { reviewApplication } from './helpers/review-application.ts';
 
 export const applicationWorker = registerWorker(
   'application',
   ApplicationBullJob,
   async (job) => {
     return match(job)
-      .with({ name: 'application.accepted' }, ({ data }) => {
-        return onApplicationAccepted(data);
-      })
       .with({ name: 'application.created' }, ({ data }) => {
-        return onApplicationCreated(data);
+        return createApplication(data);
+      })
+      .with({ name: 'application.accepted' }, ({ data }) => {
+        return acceptApplication(data);
       })
       .with({ name: 'application.rejected' }, ({ data }) => {
-        return onApplicationRejected(data);
-      })
-      .with({ name: 'application.review' }, ({ data }) => {
-        return reviewApplication(data);
+        return rejectApplication(data);
       })
       .exhaustive();
   }
